2f691d765375a08bc348fe6f09c04d29bc9ea7d9
devvratbhardwaj <bhardwaj.devvrat@gmail.com>
2025-02-10 13:30:56 -0500

    Version and dependency  changes

--- setup.py
+++ setup.py
@@ -8,10 +8,10 @@ setup(
     name='aimon-llamaindex',
     python_requires='>3.8.0',
     packages=find_packages(),
-    version="0.0.5",
+    version="0.0.6",
     install_requires=[
         "aimon==0.9.0",
-        "llama-index==0.12.15",
+        "llama-index==0.12.12",
     ],
     author='AIMon',
     author_email='info@aimon.ai',
